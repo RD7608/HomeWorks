cbed982f291838aa952c4ec093cd5b253dd7317c
Анатолий <reddragon76@yandex.ru>
2024-04-09 23:40:15 +0500

    дз4-готов

---
+++ Module2/hw4.py
@@ -0,0 +1,8 @@
+my_auto = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+
+for i in range(len(my_auto)):
+    print('Я езжу на автомобиле марки', my_auto[i])
+    cars_count += 10
+
+print(cars_count)
