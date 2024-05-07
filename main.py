4ef304d78ff59b38fb5dd0b61b9cf450b0be6b36
Анатолий <reddragon76@yandex.ru>
2024-05-08 02:33:16 +0500

    m3-hw3

---
+++ main.py
@@ -0,0 +1,15 @@
+def test(*args):
+    for i in range(len(args)):
+        print("Аргумент", i, '=', args[i])
+
+test(10, "строка", 55, [5, 4, 3], False)
+
+
+# функция факториал
+def factor(n):
+    if n == 1:
+        return 1
+    return factor(n - 1) * n
+
+
+print(factor(10))
