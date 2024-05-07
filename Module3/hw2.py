7704be3bbf71bb5088db0eeb1d008f855a1d2abf
Анатолий <reddragon76@yandex.ru>
2024-05-08 02:11:15 +0500

    m3-hw21

--- Module3/hw2.py
+++ Module3/hw2.py
@@ -2,19 +2,22 @@ def print_params(a=1, b='строка', c=True):
     print(a, b, c)
 
 
+# Вызов с разным числом параметров
 print_params()
 print_params(10, "привет", False)
 print_params(5, c=False)
 print_params(b="---------------")
 
+# Проверка
 print_params(b=25)
 print_params(c=[1, 2, 3])
 
+# Распаковка параметров
 values_list = [55, "СТР", 77.77]
 values_dict = {"a": 66, "b": 'AAA', "c": 5555}
 print_params(*values_list)
 print_params(**values_dict)
 
-
+# Распаковка + отдельные параметры
 values_list_2 = [99, "СТРОКА"]
 print_params(*values_list_2, 42)
