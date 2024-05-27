fa4fd4eb58f1cd59b476d869feaa380b2f61a6d5
RD7608 <reddragon76@yandex.ru>
2024-05-27 15:33:08 +0500

    m7

---
+++ Module7/hw1.py
@@ -0,0 +1,31 @@
+# Создание текстового файла и запись в него текста
+file_path = "my_file_1.txt"
+f = open(file_path, "w", encoding="utf-8")
+f.write("""# -*- coding: utf-8 -*-
+My soul is dark - Oh! quickly string
+The harp I yet can brook to hear;
+And let thy gentle fingers fling
+Its melting murmurs o'er mine ear.
+If in this heart a hope be dear,
+That sound shall charm it forth again:
+If in these eyes there lurk a tear,
+'Twill flow, and cease to burn my brain.
+
+But bid the strain be wild and deep,
+Nor let thy notes of joy be first:
+I tell thee, minstrel, I must weep,
+Or else this heavy heart will burst;
+For it hath been by sorrow nursed,
+And ached in sleepless silence, long;
+And now 'tis doomed to know the worst,
+And break at once - or yield to song.""")
+# Закрытие файла
+f.close()
+
+# Чтение содержимого и вывод в консоль
+f = open(file_path, "r", encoding="utf-8")
+contents = f.read()
+print(contents)
+
+# Закрытие файла
+f.close()
---
+++ Module7/hw2.py
@@ -0,0 +1,26 @@
+# Создание текстового файла и запись в него текста
+file_path = "my_file_2.txt"
+with open(file_path, "w", encoding="utf-8") as f:
+    f.write("""# -*- coding: utf-8 -*-
+My soul is dark - Oh! quickly string
+The harp I yet can brook to hear;
+And let thy gentle fingers fling
+Its melting murmurs o'er mine ear.
+If in this heart a hope be dear,
+That sound shall charm it forth again:
+If in these eyes there lurk a tear,
+'Twill flow, and cease to burn my brain.
+
+But bid the strain be wild and deep,
+Nor let thy notes of joy be first:
+I tell thee, minstrel, I must weep,
+Or else this heavy heart will burst;
+For it hath been by sorrow nursed,
+And ached in sleepless silence, long;
+And now 'tis doomed to know the worst,
+And break at once - or yield to song.""")
+
+# Чтение содержимого и вывод в консоль
+with open(file_path, "r", encoding="utf-8") as f:
+    contents = f.read()
+    print(contents)
