9c7231854a362a9df2aaa85a34abe4a31966383c
RD7608 <reddragon76@yandex.ru>
2024-06-04 12:29:24 +0500

    m4-2

--- Module4/module_4_2.py
+++ Module4/module_4_2.py
@@ -4,8 +4,12 @@ def test_function():
 
     inner_function()
 
+
 # Вызов функции test_function
 test_function()
 
 # Попытка вызова inner_function вне функции test_function
 # inner_function()
+
+# При попытке вызова inner_function вне функции test_function возникнет ошибка,
+# так как inner_function определена только внутри test_function.
