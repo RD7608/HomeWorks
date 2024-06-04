385d8c42235c3dc056b7373ac395dafbf16f8112
RD7608 <reddragon76@yandex.ru>
2024-06-04 12:25:33 +0500

    m4-2

---
+++ Module4/module_4_2.py
@@ -0,0 +1,11 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()
+
+# Вызов функции test_function
+test_function()
+
+# Попытка вызова inner_function вне функции test_function
+# inner_function()
