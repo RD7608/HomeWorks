6f2e97783a56722c141b07fb50ee1d1d30035e81
RD7608 <reddragon76@yandex.ru>
2024-06-06 15:57:47 +0500

    m3-h

---
+++ Module3/module3hard.py
@@ -0,0 +1,26 @@
+def calculate_structure_sum(data_structure):
+    def calculate(item):
+        if isinstance(item, int):
+            return item
+        elif isinstance(item, str):
+            return len(item)
+        elif isinstance(item, (list, tuple)):
+            return sum(calculate(subitem) for subitem in item)
+        elif isinstance(item, dict):
+            return sum(calculate(key) + calculate(value) for key, value in item.items())
+        else:
+            return 0
+
+    return sum(calculate(item) for item in data_structure)
+
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+result = calculate_structure_sum(data_structure)
+print(result)
