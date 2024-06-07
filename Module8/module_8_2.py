7fc69b10e0bab20c36af2c3d9a9515d487699b60
RD7608 <reddragon76@yandex.ru>
2024-06-07 11:42:52 +0500

    m8-2

---
+++ Module8/module_8_2.py
@@ -0,0 +1,33 @@
+class InvalidDataException(Exception):
+    pass
+
+
+class ProcessingException(Exception):
+    pass
+
+
+def process_data(value):
+    try:
+        if value < 0:
+            raise InvalidDataException("значение меньше нуля")
+        elif value == 0:
+            raise ProcessingException("значение равно нулю")
+        else:
+            return value * 2
+    except InvalidDataException as e:
+        print(f"Ошибка: {e}")
+        raise
+    except ProcessingException as e:
+        print(f"Ошибка обработки: {e}")
+        raise
+
+
+try:
+    result = process_data(0)
+    print(f"Результат обработки данных: {result}")
+except InvalidDataException as e:
+    print(f"Ошибка в данных: {e}")
+except ProcessingException as e:
+    print(f"Ошибка обработки данных: {e}")
+except Exception as e:
+    print(f"Неожиданная ошибка: {e}")
