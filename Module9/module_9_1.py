adb7ebc8e1fc5f516767e121e995a73009f8917e
RD7608 <reddragon76@yandex.ru>
2024-06-10 12:02:11 +0500

    m9-1

--- Module9/module_9_1.py
+++ Module9/module_9_1.py
@@ -1,7 +1,19 @@
+# функция для фильтрации нечетных чисел
+def is_odd(x):
+    return x % 2 != 0
+
+# функция для возведения числа в квадрат
+def square(x):
+    return x ** 2
+
 # Входные данные
 numbers = [1, 2, 5, 7, 12, 11, 35, 4, 89, 10]
 
-# Фильтрация нечётных чисел и возведение их в квадрат
-result = list(map(lambda x: x ** 2, filter(lambda x: x % 2 != 0, numbers)))
+# Фильтрация нечетных чисел и возведение их в квадрат
+filtered_numbers = filter(is_odd, numbers)
+result = map(square, filtered_numbers)
 
-print(result)  # Вывод результата
+# Преобразование результата в список
+result_list = list(result)
+
+print(result_list)
