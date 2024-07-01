0099765d4a7d2e63cff79765f0fb9512ac4851fd
Анатолий <reddragon76@yandex.ru>
2024-07-02 01:35:57 +0500

    m11-1

--- Module_11/module_11_1.py
+++ Module_11/module_11_1.py
@@ -1,8 +1,22 @@
-# подключаем pandas и matplotlib
+# подключаем numpy, pandas и matplotlib
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
+# создаем массив на 10 лет
+y = 2014
+years = np.arange(y, y+10, 1)
+
+# добавляем к годам продажи (здесь случайные значения в диапазоне)
+sales_dict = {"Год": years,
+              "Продажи": np.random.randint(400, 1300, size=len(years))}
+
+# создаем таблицу из продаж
+sales = pd.DataFrame(sales_dict)
+
+# записываем таблицу в файл
+sales.to_csv('data.csv', index=False)
 
 # Считываем данные из CSV файла (продажи по годам)
 df = pd.read_csv('data.csv')
@@ -13,8 +27,9 @@ print(df[(df["Год"] == 2023)].head())
 # вычисляем среднее значение столбца "Продажи")
 mean_value = df['Продажи'].mean()
 
-# Выводим результаты анализа
+# Выводим результат
 print(f"\nСреднее значение продаж за {len(df)} лет :", mean_value)
+
 # print(df.describe())
 
 
@@ -22,26 +37,29 @@ print(f"\nСреднее значение продаж за {len(df)} лет :",
 # задаем стиль графика
 mpl.style.use(['Solarize_Light2'])
 
-# зададим цвет в зависимости от величины 'Продажи' (>1000 - зел, <500 - красный, иначе - синий)
+# зададим цвет в зависимости от величины в колонке 'Продажи' (>1000 - зел, <500 - красный, иначе - синий)
 df['Цвет'] = df['Продажи'].apply(lambda x: 'green' if x > 1000 else ('red' if x < 500 else 'blue'))
 
 # Создаем график
 fig, ax = plt.subplots(figsize=(10, 6))
 # линейный
-plt.plot(df['Год'], df['Продажи'], marker='o', color='green', linestyle='-', linewidth=2)
+plt.plot(df['Год'], df['Продажи'], marker='o', color='black', linestyle='-', linewidth=2)
 # столбчатый
-bar = plt.bar(df['Год'], df['Продажи'], align='center', alpha=0.5, color=df['Цвет'])
+bar = plt.bar(df['Год'], df['Продажи'], width=0.6, align='center', alpha=0.5, color=df['Цвет'])
 
 # Добавляем заголовок и подписи к осям
 plt.title('Динамика продаж по годам')
 plt.xlabel('Год')
 plt.ylabel('Продажи тыс. руб.')
 
-# убиравем сетку на графике
+# убираем сетку на графике
 plt.grid(False)
 
-# добавляем значения на график в центр столбцов
+# добавляем значения 'Продажи' на график в центр столбцов
 ax.bar_label(bar, label_type='center')
+
+# настраиваем подписи столбцов
+plt.xticks(years, fontsize=14, rotation=60)
 
 # Отображаем график
 plt.show()
