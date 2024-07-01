09f60613f66324684211a32534bb78711c3a3a30
RD7608 <reddragon76@yandex.ru>
2024-07-01 13:34:39 +0500

    m11-1

--- Module_11/module_11_1.py
+++ Module_11/module_11_1.py
@@ -1,6 +1,7 @@
 # подключаем pandas и matplotlib
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 
 # Считываем данные из CSV файла (продажи по годам)
@@ -14,20 +15,34 @@ mean_value = df['Продажи'].mean()
 
 # Выводим результаты анализа
 print(f"\nСреднее значение продаж за {len(df)} лет :", mean_value)
+# print(df.describe())
 
 
 # Построим график продаж по годам с помощью matplotlib
-# Создаем график линий
-plt.figure(figsize=(10, 6))
-plt.plot(df['Год'], df['Продажи'], marker='o', color='b', linestyle='-', linewidth=2)
+# задаем стиль графика
+mpl.style.use(['Solarize_Light2'])
+
+# зададим цвет в зависимости от величины 'Продажи' (>1000 - зел, <500 - красный, иначе - синий)
+df['Цвет'] = df['Продажи'].apply(lambda x: 'green' if x > 1000 else ('red' if x < 500 else 'blue'))
+
+# Создаем график
+fig, ax = plt.subplots(figsize=(10, 6))
+# линейный
+plt.plot(df['Год'], df['Продажи'], marker='o', color='green', linestyle='-', linewidth=2)
+
+# столбчатый
+bar = plt.bar(df['Год'], df['Продажи'], align='center', alpha=0.5, color=df['Цвет'])
 
 # Добавляем заголовок и подписи к осям
 plt.title('Динамика продаж по годам')
 plt.xlabel('Год')
 plt.ylabel('Продажи тыс. руб.')
 
-# Добавляем сетку на график
-plt.grid(True)
+# убиравем сетку на графике
+plt.grid(False)
+
+# добавляем значения на график в центр столбцов
+ax.bar_label(bar, label_type='center')
 
 # Отображаем график
 plt.show()
