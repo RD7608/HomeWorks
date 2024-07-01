f721eb31cbeda43f40c787dfb59966fc752017ff
RD7608 <reddragon76@yandex.ru>
2024-07-01 11:31:10 +0500

    m11-1

--- Module_11/module_11_1.py
+++ Module_11/module_11_1.py
@@ -1,49 +1,33 @@
-#import requests
-
-# Отправляем GET-запрос к API
-#response = requests.get('https://jsonplaceholder.typicode.com/posts')
-
-# Проверяем статус код ответа
-#if response.status_code == 200:
-#    data = response.json()  # Преобразуем полученный ответ в формат JSON
-#    for post in data:
-#        print(f"Post #{post['id']}: {post['title']}")  # Выводим название каждого поста в консоль
-#else:
-#    print("Ошибка при запросе данных. Статус код:", response.status_code)
-
+# подключаем pandas и matplotlib
 import pandas as pd
-
-# Считываем данные из CSV файла
-df = pd.read_csv('data.csv')  # Замените 'data.csv' на название вашего файла
-
-# Выводим первые несколько строк данных для предварительного анализа
-print("Первые строки данных:")
-print(df.head())
-print(df.columns.tolist ())
-
-# Выполняем простой анализ данных (например, вычисляем среднее значение числового столбца)
-mean_value = df['Продажи'].mean()  # Замените 'column_name' на название столбца, который вы хотите проанализировать
-
-# Выводим результаты анализа
-print("\nСреднее значение числового столбца:", mean_value)
-
-
 import matplotlib.pyplot as plt
 
-# Считываем данные из CSV файла
-#df = pd.read_csv('data.csv')  # Убедитесь, что файл содержит столбцы 'Год' и 'Продажи'
 
+# Считываем данные из CSV файла (продажи по годам)
+df = pd.read_csv('data.csv')
+
+# Выводим строку за 2023 год
+print(df[(df["Год"] == 2023)].head())
+
+# вычисляем среднее значение столбца "Продажи")
+mean_value = df['Продажи'].mean()
+
+# Выводим результаты анализа
+print(f"\nСреднее значение продаж за {len(df)} лет :", mean_value)
+
+
+# Построим график продаж по годам с помощью matplotlib
 # Создаем график линий
-#plt.figure(figsize=(10, 6))
-#plt.plot(df['Year'], df['Sales'], marker='o', color='b', linestyle='-', linewidth=2)
+plt.figure(figsize=(10, 6))
+plt.plot(df['Год'], df['Продажи'], marker='o', color='b', linestyle='-', linewidth=2)
 
 # Добавляем заголовок и подписи к осям
-#plt.title('Динамика продаж по годам')
-#plt.xlabel('Year')
-#plt.ylabel('Sales')
+plt.title('Динамика продаж по годам')
+plt.xlabel('Год')
+plt.ylabel('Продажи тыс. руб.')
 
 # Добавляем сетку на график
-#plt.grid(True)
+plt.grid(True)
 
 # Отображаем график
-#plt.show()
+plt.show()
