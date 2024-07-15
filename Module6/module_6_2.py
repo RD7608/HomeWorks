93b94aff8d763bcc88e085fa8cc05b245690b0d3
RD7608 <reddragon76@yandex.ru>
2024-07-15 13:30:52 +0500

    m6-2

--- Module6/module_6_2.py
+++ Module6/module_6_2.py
@@ -1,28 +1,51 @@
 class Vehicle:
-    def __init__(self):
-        self.vehicle_type = "none"
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self, owner, model, color, engine_power):
+        self.owner = owner
+        self.__model = model
+        self.__color = color
+        self.__engine_power = engine_power
+
+    def get_model(self):
+        return f"Модель: {self.__model}"
+
+    def get_horsepower(self):
+        return f"Мощность двигателя: {self.__engine_power}"
+
+    def get_color(self):
+        return f"Цвет: {self.__color}"
+
+    def print_info(self):
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+        print(f"Владелец: {self.owner}")
+
+    def set_color(self, new_color):
+        if new_color.lower() in self.__COLOR_VARIANTS:
+            self.__color = new_color
+        else:
+            print(f"Нельзя сменить цвет на {new_color}")
 
 
-class Car(Vehicle):
-    def __init__(self):
-        super().__init__()
-        self.price = 1000000
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
 
-    def horse_powers(self):
-        return 200  # у всех автомобилей 200 лошадиных сил
+    def __init__(self, owner, model, color, engine_power):
+        super().__init__(owner, model, color, engine_power)
 
 
-class Nissan(Car):
-    def __init__(self):
-        super().__init__()
-        self.vehicle_type = "легковой универсал"
-        self.price = 1500000  # переопределяем цену для Nissan
+# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
 
-    def horse_powers(self):
-        return 250  # переопределяем ЛС для Nissan
+# Изначальные свойства
+vehicle1.print_info()
 
+# Меняем свойства (в т.ч. вызывая методы)
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
 
-nissan = Nissan()
-print(nissan.vehicle_type, nissan.price)
-
-print("ЛC:", nissan.horse_powers())
+# Проверяем что поменялось
+vehicle1.print_info()
