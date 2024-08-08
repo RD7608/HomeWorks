3488cf387ff7ac68fae4e9ae8640ce93b043d232
Анатолий <reddragon76@yandex.ru>
2024-08-08 18:10:25 +0500

    m13-5

---
+++ module_13/module_13_5.py
@@ -0,0 +1,71 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+import asyncio
+
+
+api = ''
+
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ['Рассчитать', 'Информация']
+    keyboard.add(*[types.KeyboardButton(text) for text in buttons])
+    await message.answer('Привет! Я бот, помогающий твоему здоровью.\nВыберите действие:', reply_markup=keyboard)
+
+
+@dp.message_handler(lambda message: message.text == 'Рассчитать', state=None)
+async def set_age(message: types.Message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=int(message.text))
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=int(message.text))
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=int(message.text))
+    data = await state.get_data()
+
+    # Вычисление нормы калорий по упрощенной формуле Миффлина - Сан Жеора для мужчин
+    if data['growth'] > 0 and data['weight'] > 0 and data['age'] > 0:
+        calories = 10 * data['weight'] + 6.25 * data['growth'] - 5 * data['age'] + 5
+        await message.answer(f'Ваша норма калорий: {int(calories)} ккал/день')
+    else:
+        await message.answer('Не удалось рассчитать норму калорий. Проверьте введенные данные.')
+
+    await state.finish()
+
+
+@dp.message_handler(lambda message: message.text == 'Информация')
+async def show_info(message: types.Message):
+    info_text = "Бот позволяет рассчитать вашу суточную норму калорий " \
+                "по упрощенной формуле Миффлина - Сан Жеора для мужчин. Для этого вам нужно " \
+                "ввести ваш возраст, рост и вес."
+    await message.answer(info_text)\
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
