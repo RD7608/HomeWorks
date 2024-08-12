bde5bee2b1da18b3eb0ac636842f983ed263c07d
Анатолий <reddragon76@yandex.ru>
2024-08-12 11:16:34 +0500

    m8-2

--- Module8/module_8_2.py
+++ Module8/module_8_2.py
@@ -11,24 +11,30 @@ def personal_sum(numbers):
             result += num
         except TypeError:
             incorrect_data += 1
+            print(f'Некорректный тип данных для подсчёта суммы : {num}')
 
     return result, incorrect_data
 
 
 def calculate_average(numbers):
-    if not isinstance(numbers, (list, tuple)):
+    if not isinstance(numbers, (list, tuple, str)):
         print('В numbers записан некорректный тип данных')
         return None
 
+    if isinstance(numbers, str):
+        numbers = list(numbers)
+
     try:
-        sum_result, _ = personal_sum(numbers)
-        avg = sum_result / len(numbers)
+        sum_result, incorrect_data = personal_sum(numbers)
+        if len(numbers) - incorrect_data == 0:
+            return 0
+        avg = sum_result / (len(numbers) - incorrect_data)
         return avg
     except ZeroDivisionError:
         return 0
 
 
-print(f'Результат 1: {calculate_average(["1, 2, 3"])}')
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
 print(f'Результат 3: {calculate_average(567)}')
 print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
