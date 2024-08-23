ace9f613ca87f30c241497d55031da7b15d12594
Анатолий <reddragon76@yandex.ru>
2024-08-23 09:04:53 +0500

    m16-2

---
+++ module_16/module_16_2.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+from pydantic import constr
+
+app = FastAPI()
+
+@app.get("/")
+def main_page():
+    return {"message": "Главная страница"}
+
+@app.get("/user/admin")
+def admin_page():
+    return {"message": "Вы вошли как администратор"}
+
+
+@app.get("/user/{username}/{age}")
+def user_info(
+    username: Annotated[constr(min_length=5, max_length=20), Path(description="Enter username", examples=["UrbanUser"])],
+    age: Annotated[int, Path(ge=18, le=120, description="Enter age", examples=[24])]
+):
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+
+@app.get("/user/{user_id}")
+def user_page(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", examples=[1])]):
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
