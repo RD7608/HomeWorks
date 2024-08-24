76980bb604b0c340f846ed6ecd2a539984bf6471
Анатолий <reddragon76@yandex.ru>
2024-08-24 12:07:52 +0500

    m16

--- module_16/module_16_2.py
+++ module_16/module_16_2.py
@@ -1,28 +1,28 @@
 from fastapi import FastAPI, Path
 from typing import Annotated
-from pydantic import constr
+
 
 app = FastAPI()
 
 
 @app.get("/")
-def main_page():
+async def main_page():
     return {"message": "Главная страница"}
 
 
 @app.get("/user/admin")
-def admin_page():
+async def admin_page():
     return {"message": "Вы вошли как администратор"}
 
 
 @app.get("/user/{user_id}")
-def user_page(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example=1)]):
+async def user_page(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example=1)]):
     return {"message": f"Вы вошли как пользователь № {user_id}"}
 
 
 @app.get("/user/{username}/{age}")
-def user_info(
-    username: Annotated[constr(min_length=5, max_length=20), Path(description="Enter username", example="UrbanUser")],
+async def user_info(
+    username: Annotated[str, Path(description="Enter username", example="UrbanUser", min_length=5, max_length=20)],
     age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=24)]
 ):
     return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
--- module_16/module_16_3.py
+++ module_16/module_16_3.py
@@ -1,49 +1,52 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path
 from typing import Annotated
 
-app = FastAPI()
 
+app = FastAPI()
 
 users = {'1': 'Имя: Example, возраст: 18'}
 
 
 @app.get("/users")
-def get_users():
+async def get_users():
     return users
 
 
 @app.post("/user/{username}/{age}")
-def create_user(
+async def create_user(
     username: Annotated[str, Path(description="Enter username", example="UrbanUser", min_length=5, max_length=20)],
     age: Annotated[int, Path(description="Enter age", example=24, ge=18, le=120)]
 ):
-    new_id = str(len(users) + 1)
+    if users:
+        new_id = str(max(map(int, users.keys())) + 1)
+    else:
+        new_id = '1'
+    if int(new_id) > 100:
+        return f"User not created. ID {new_id} is too big"
     users[new_id] = f"Имя: {username}, возраст: {age}"
     return f"User {new_id} is registered"
 
 
 @app.put("/user/{user_id}/{username}/{age}")
-def update_user(
+async def update_user(
     user_id: Annotated[int, Path(description="Enter User ID", example=1, ge=1, le=100)],
     username: Annotated[str, Path(description="Enter username", example="UrbanProfi", min_length=5, max_length=20)],
     age: Annotated[int, Path(description="Enter age", example=28, ge=18, le=120)]
 ):
     user_id = str(user_id)
     if user_id not in users:
-        raise HTTPException(status_code=404, detail=f"User {user_id} not found")
-
+        return f"User {user_id} not found"
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"User {user_id} has been updated"
 
 
 @app.delete("/user/{user_id}")
-def delete_user(
+async def delete_user(
     user_id: Annotated[int, Path(description="Enter User ID", example=2, ge=1, le=100)]
 ):
     user_id = str(user_id)
     if user_id not in users:
-        raise HTTPException(status_code=404, detail=f"User {user_id} not found")
-
+        return f"User {user_id} not found"
     del users[user_id]
     return f"User {user_id} has been deleted"
 
