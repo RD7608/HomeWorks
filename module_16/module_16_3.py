f814b389025baac19d506874345504747b63748e
Анатолий <reddragon76@yandex.ru>
2024-08-24 12:43:18 +0500

    m9-1

--- module_16/module_16_3.py
+++ module_16/module_16_3.py
@@ -8,7 +8,7 @@ users = {'1': 'Имя: Example, возраст: 18'}
 
 
 @app.get("/users")
-async def get_users():
+async def get_users() -> dict:
     return users
 
 
@@ -16,9 +16,9 @@ async def get_users():
 async def create_user(
     username: Annotated[str, Path(description="Enter username", example="UrbanUser", min_length=5, max_length=20)],
     age: Annotated[int, Path(description="Enter age", example=24, ge=18, le=120)]
-):
+) -> str:
     if users:
-        new_id = str(max(map(int, users.keys())) + 1)
+        new_id = str(int(max(users, key=int)) + 1)
     else:
         new_id = '1'
     if int(new_id) > 100:
@@ -32,7 +32,7 @@ async def update_user(
     user_id: Annotated[int, Path(description="Enter User ID", example=1, ge=1, le=100)],
     username: Annotated[str, Path(description="Enter username", example="UrbanProfi", min_length=5, max_length=20)],
     age: Annotated[int, Path(description="Enter age", example=28, ge=18, le=120)]
-):
+) -> str:
     user_id = str(user_id)
     if user_id not in users:
         return f"User {user_id} not found"
@@ -43,12 +43,18 @@ async def update_user(
 @app.delete("/user/{user_id}")
 async def delete_user(
     user_id: Annotated[int, Path(description="Enter User ID", example=2, ge=1, le=100)]
-):
+) -> str:
     user_id = str(user_id)
     if user_id not in users:
         return f"User {user_id} not found"
-    del users[user_id]
+    users.pop(user_id)
     return f"User {user_id} has been deleted"
+
+
+@app.delete("/")
+async def delete_all_user() -> str:
+    users.clear()
+    return f"Users cleared"
 
 
 if __name__ == "__main__":
--- module_16/test.py
+++ module_16/test.py
@@ -3,23 +3,19 @@ import requests
 # 1. GET /users
 response = requests.get("http://127.0.0.1:8000/users")
 print(1, response.json())
-
 # 2. POST /user/{username}/{age} (username - UrbanUser, age - 24)
 response = requests.post("http://127.0.0.1:8000/user/UrbanUser/24")
 print(2, response.text)
-
 # 3. POST /user/{username}/{age} (username - NewUser, age - 22)
 response = requests.post("http://127.0.0.1:8000/user/NewUser/22")
 print(3, response.text)
-
 # 4. PUT /user/{user_id}/{username}/{age} (user_id - 1, username - UrbanProfi, age - 28)
 response = requests.put("http://127.0.0.1:8000/user/1/UrbanProfi/28")
 print(4, response.text)
-
 # 5. DELETE /user/{user_id} (user_id - 2)
 response = requests.delete("http://127.0.0.1:8000/user/2")
 print(5, response.text)
-
 # 6. GET /users
 response = requests.get("http://127.0.0.1:8000/users")
 print(6, response.json())
+
