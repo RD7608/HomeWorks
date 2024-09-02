a546957c8b83ae2e7ca4a5059ad1ccf5aac7ace8
RD7608 <reddragon76@yandex.ru>
2024-09-02 10:06:08 +0500

    m7-2

--- Module7/module_7_2.py
+++ Module7/module_7_2.py
@@ -2,10 +2,10 @@ def custom_write(file_name, strings):
     strings_positions = {}
 
     with open(file_name, 'w', encoding='utf-8') as file:
-        for string in strings:
+        for index, string in enumerate(strings):
             start_position = file.tell()
             file.write(string + '\n')
-            strings_positions[(strings.index(string) + 1, start_position)] = string
+            strings_positions[(index + 1, start_position)] = string
 
     return strings_positions
 
@@ -19,5 +19,11 @@ info = [
 ]
 
 result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
+
+
+strings = ['a', 'a', 'a', 'v', 'v']
+result = custom_write('test.txt', strings)
 for elem in result.items():
     print(elem)
