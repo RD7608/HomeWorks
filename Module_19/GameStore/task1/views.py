d0ab2fa501da3db47efe7da63ae2ac3637934610
Анатолий <reddragon76@yandex.ru>
2024-09-22 18:01:37 +0500

    m19-"Пагинация"

--- Module_19/GameStore/task1/views.py
+++ Module_19/GameStore/task1/views.py
@@ -1,23 +1,42 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import *
 from .forms import *
 
-Buyers = Buyer.objects.all()
+# Buyers = Buyer.objects.all()
 
-existing_users = []
-for each in Buyers:
-    existing_users.append(each.name)
+# existing_users = []
+# for each in Buyers:
+#    existing_users.append(each.name)
+
+existing_users = set(Buyer.objects.values_list('name', flat=True))  # Список всех пользователей
+
 
 def index(request):
     return render(request, 'task1/index.html')
 
 
 def shop(request):
-    Games = Game.objects.all()
+    page_size = request.GET.get('page_size', '3')  # Получение размера страницы
+    games = Game.objects.all().order_by('id')  # Все игры, отсортированные по id
+
+    if page_size == 'all':
+        page_obj = games
+    else:
+        try:
+            page_size = int(page_size)  # Преобразование в целое число
+        except (ValueError, TypeError):
+            page_size = 3  # Значение по умолчанию в случае ошибки
+
+        paginator = Paginator(games, page_size)  # Пагинация с размером страницы
+        page_number = request.GET.get('page', 1)  # Номер страницы
+        page_obj = paginator.get_page(page_number)  # Пагинация
+
     context = {
-        'Games': Games,
+        'page_obj': page_obj,
+        'page_size': page_size,
     }
-    return render(request, 'task1/shop.html', context)
+    return render(request, 'task1/shop.html', context=context)
 
 
 def cart(request):
@@ -31,16 +50,16 @@ def sign_up(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
-#            password = form.cleaned_data['password']
-#            repeat_password = form.cleaned_data['repeat_password']
+            #            password = form.cleaned_data['password']
+            #            repeat_password = form.cleaned_data['repeat_password']
             age = form.cleaned_data['age']
 
             if username in existing_users:
                 info['error'] = "Пользователь с таким именем уже существует."
-#            elif password != repeat_password:
-#                info['error'] = "Пароли не совпадают."
-#            elif age < 18:
-#                info['error'] = 'Вы должны быть старше 18'
+            #            elif password != repeat_password:
+            #                info['error'] = "Пароли не совпадают."
+            #            elif age < 18:
+            #                info['error'] = 'Вы должны быть старше 18'
             else:
                 Buyer.objects.create(name=username, age=age)
                 info['success'] = f'Приветствуем, {username}!'
@@ -48,7 +67,6 @@ def sign_up(request):
             info['error'] = "Ошибка в обработке формы."
     else:
         form = RegistrationForm()
-
 
     info['form'] = form
     return render(request, 'task1/registration_page.html', context=info)
